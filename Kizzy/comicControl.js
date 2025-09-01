4b04c65d3c53674500897101f1f970fc6d108573
Kelly Evans <Evans.kelly92@gmail.com>
2025-09-01 10:37:31 +0100

    Add files via upload
    Adds keyboard support

---
+++ Kizzy/comicControl.js
@@ -0,0 +1,35 @@
+var pageNo = 1;
+var bgChange = document.getElementById('body');
+
+document.addEventListener("keyup", e => {
+    const k = e.key;
+    if (k == "ArrowRight" || k == " " || k == "d")
+    {
+        next();
+    }
+    else if (k == "ArrowLeft" || k == "Backspace" || k == "a")
+    {
+        prev();
+    }
+ });
+
+function next() {
+    pageNo++;
+    document.getElementById("page").src = pageNo + '.jpg';
+    window.scrollTo(0, 0);
+}
+
+function prev() {
+    pageNo--;
+
+    if (pageNo < 1){
+        pageNo = 1;
+    }
+    document.getElementById("page").src = pageNo + '.jpg';
+    window.scrollTo(0, 0);
+}
+
+function end() {
+    pageNo--;
+    document.getElementById("page").src = pageNo + '.jpg';
+}
