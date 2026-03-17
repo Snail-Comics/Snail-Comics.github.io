cc8d19b159ba2b3c8c6f19f79d07ac4b2ca81d68
Kelly Evans <Evans.kelly92@gmail.com>
2026-03-17 11:46:02 +0000

    Add files via upload
    Removes unique funtions meant for Summerleagues comic
    Changes pages from .png to .jpg

--- IslandOCT/comicControl.js
+++ IslandOCT/comicControl.js
@@ -1,6 +1,5 @@
 var pageNo = 1;
 var bgChange = document.getElementById('body');
-var folder = 0;
 var display = "Aud/";
 
 //keyboard support
@@ -18,108 +17,21 @@ document.addEventListener("keyup", e => {
 
 function next() {
     pageNo++;
-    document.getElementById("page").src = display + pageNo + '.png';
+    document.getElementById("page").src = display + pageNo + '.jpg';
     window.scrollTo(0, 0);
-    //checks which round it is
-    if (folder == 1){
-        backgroundChange();
-    } 
-    else {
-    spookMode();
-    }
 }
 
 function prev() {
     pageNo--;
-
-    if (pageNo < 1 && folder == 0){
+    if (pageNo == 0) {
         pageNo = 1;
     }
-    //goes back to Audition last page
-    if (pageNo < 1 && folder == 1) {
-        folder = 0;
-        display = "Aud/"
-        pageNo = 61;
-    }
-    document.getElementById("page").src = display + pageNo + '.png';
+    document.getElementById("page").src = display + pageNo + '.jpg';
     window.scrollTo(0, 0);
-    if (folder == 1){
-        backgroundChange();
-    } 
-    else {
-    spookMode();
-    }
 }
 
 function end() {
-    //changes to next folder
-    if (folder == 0)
-    {
-        folder = 1;
-        pageNo = 1;
-        display = "R1/";
-        document.getElementById("page").src = display + pageNo + '.png';
-    }
-    else{
-        pageNo--;
-        document.getElementById("page").src = display + pageNo + '.png';
-    }
-    
-}
+    pageNo--;
+    document.getElementById("page").src = display + pageNo + '.jpg';
 
-
-//These are background effects for the comic
-function spookMode() {
-    if (pageNo >= 25 && pageNo <= 39)
-    {
-        bgChange.classList.remove("normal");
-        bgChange.classList.add("carcosa");
-    }
-    else 
-    {
-        bgChange.classList.remove("carcosa");
-        bgChange.classList.add("normal");
-    }
-}
-
-function backgroundChange ()
-{
-    switch (pageNo) {
-        case 55:
-            bgChange.classList.remove("carcosa");
-            bgChange.classList.add("normal");
-            break;
-        
-        case 56: 
-            bgChange.classList.remove("normal");
-            bgChange.classList.add("carcosa"); 
-            break;
-
-        case 69: 
-            bgChange.classList.remove("normal");
-            bgChange.classList.add("carcosa"); 
-            break;
-
-        case 70:
-            bgChange.classList.remove("carcosa");
-            bgChange.classList.add("normal");
-            break;
-
-        case 76:
-            bgChange.classList.remove("sploosh");
-            bgChange.classList.add("normal"); 
-            break;
-
-        case 77:
-            bgChange.classList.remove("normal");
-            bgChange.classList.add("sploosh"); 
-            break;
-
-        case 78:
-            bgChange.classList.remove("sploosh");
-            bgChange.classList.add("normal"); 
-            break;
-
-        default: break;
-    }
 }
