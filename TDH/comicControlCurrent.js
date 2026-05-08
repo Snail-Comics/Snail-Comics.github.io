296b8917afe18eec1372f9b3ca45bdf264e92e90
Kelly Evans <Evans.kelly92@gmail.com>
2026-05-08 13:18:11 +0100

    Add files via upload
    js for the current part

---
+++ TDH/comicControlCurrent.js
@@ -0,0 +1,88 @@
+var pageNo = 1;
+var displayNo = 1;
+
+const CHlength = [63, 35, 16, 24]
+
+//keyboard support
+document.addEventListener("keyup", e => {
+    const k = e.key;
+    if (k == "ArrowRight" || k == "w")
+    {
+        next();
+    }
+    else if (k == "ArrowLeft" || k == "s")
+    {
+        prev();
+    }
+ });
+
+function next() {
+    pageNo++;
+    getChapter();
+    pageSwitch();
+    document.getElementById("page").src = pageNo + '.jpg';
+    window.scrollTo(0, 0);
+}
+
+function prev() {
+    pageNo--;
+    if (pageNo < 1){
+        getChapter();
+        var previousPart = (displayNo -1).toString();
+        var newURL = "https://snail-comics.github.io/TDH/Pages/" + previousPart + "/" + previousPart + ".html";
+        window.location.replace(newURL);
+    }
+    pageSwitch();
+    document.getElementById("page").src = pageNo + '.jpg';
+    window.scrollTo(0, 0);
+}
+
+function end() {
+    //changes to next folder 
+    getChapter();
+    if (displayNo < CHlength.length)
+    {
+        pageNo = 1;
+        displayNo++;
+        window.location.href = "../" + displayNo + "/" + displayNo + ".html";
+    }
+  else {
+      pageNo--;
+      document.getElementById("page").src = pageNo + '.jpg';
+  }
+}
+
+function getChapter(){
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    const arr = page.split(".");
+    displayNo= Number(arr[0]);
+}
+
+function pageSwitch(){
+    var backBTNChange = document.getElementById('backBTN');
+   switch (pageNo){
+    case 1:
+           backBTNChange.innerHTML = "PREVIOUS PART";
+           break;
+        case 2:
+            backBTNChange.innerHTML = "BACK";
+            break;
+       default:
+           break;
+    }
+    var nextBTNChange = document.getElementById('nextBTN');
+    var lastPanel = displayNo - 1;
+    var end = CHlength[lastPanel];
+   if (pageNo == end)
+    {
+        nextBTNChange.setAttribute("hidden", "hidden");
+    }
+   else
+   {
+        var hide = nextBTNChange.getAttribute("hidden");
+        nextBTNChange.removeAttribute(hide);
+       nextBTNChange.innerHTML = "NEXT";
+   }
+    return;
+}
